9c74a7511209f81678d8eb92a75a523766177ee1
bsqmAcademlo <brayan.munoz@academlo.com>
2022-09-19 18:58:56 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const text = document.querySelector(".text");
+const author = document.querySelector(".author");
+const box = document.querySelector(".box");
+
+async function getQuotes() {
+    try {
+        const data = await fetch("./db.json");
+        const { quotes, colors } = await data.json();
+
+        const numberQuote = numberRandom(quotes.length);
+        const numberColor = numberRandom(colors.length);
+
+        text.innerHTML = `<i>"${quotes[numberQuote].text}"</i>`;
+        text.style.color = colors[numberColor];
+
+        author.textContent = quotes[numberQuote].author;
+        author.style.color = colors[numberColor];
+
+        document.body.style.background = colors[numberColor];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function numberRandom(max) {
+    return Math.trunc(Math.random() * (max - 0) + 0);
+}
+
+box.addEventListener("click", () => {
+    getQuotes();
+});
+
+getQuotes();
